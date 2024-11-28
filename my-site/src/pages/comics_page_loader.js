7444f9957c9f48deb741875f2266898b285104d0
CoCuCoH41k <jesiyesr@gmail.com>
2024-11-28 18:15:55 +0700

    №1 Upload

---
+++ my-site/src/pages/comics_page_loader.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+
+const PRELOAD_RANGE = 2; // Кол-во страниц перед и после текущей для предзагрузки
+
+const LAST_AVALIABLE = 1239;
+
+const pagesPathURL = (id) => {
+    return `https://tkuniverse.space/ru/pages/${id}.png`
+}
+
+const cpl = (pageId, onPageChange) => {
+    const [currentPage, setCurrentPage] = useState(null);
+    const [pages, setPages] = useState({});
+    const loadPage = (id) => {
+        return new Promise((resolve) => {
+            const img = new Image();
+            img.src = pagesPathURL(id);
+            img.onload = () => resolve(img);
+        });
+    };
+
+    useEffect(() => {
+        loadPage(pageId).then((img) => {
+            setCurrentPage(img);
+            setPages((prevPages) => ({ ...prevPages, [pageId]: img }));
+        });
+
+        const preloadPages = (start, end) => {
+            for (let i = start; i <= end; i++) {
+                if (i !== pageId && !pages[i]) {
+                    loadPage(i).then((img) => {
+                        setPages((prevPages) => ({ ...prevPages, [i]: img }));
+                    });
+                }
+            }
+        };
+
+        preloadPages(pageId + 1, pageId + PRELOAD_RANGE);
+        preloadPages(pageId - PRELOAD_RANGE, pageId - 1);
+        
+    }, [pageId]);
+
+    const handleNextPage = () => {
+        if (pages[pageId + 1]) {
+            onPageChange(pageId + 1);
+        }
+    };
+
+    const handlePrevPage = () => {
+        if (pages[pageId - 1] && pageId > 1) {
+            onPageChange(pageId - 1);
+        }
+    };
+
+    const handleFirstPage = () => {
+        if (pages[1]) {
+            onPageChange(1)
+        } else {
+            loadPage(1).finally(onPageChange(1))
+        }
+    }
+
+    const handleLastPage = () => {
+        if (pages[LAST_AVALIABLE]) {
+            onPageChange(LAST_AVALIABLE)
+        } else {
+            loadPage(LAST_AVALIABLE).finally(onPageChange(LAST_AVALIABLE))
+        }
+    }
+
+    return ({
+        page_image: currentPage,
+        prev_loaded: pages[pageId - 1],
+        next_loaded: pages[pageId + 1],
+
+        nav: {
+            first: handleFirstPage,
+            prev: handlePrevPage,
+            next: handleNextPage,
+            last: handleLastPage
+        }
+    });
+};
+
+export default cpl;
---
+++ my-site/src/pages/index.js
@@ -0,0 +1,20 @@
+"use client"
+
+import react from "react";
+import Image from "next/image";
+import localFont from "next/font/local";
+import page_loader from "./comics_page_loader.js"
+import { useState } from "react";
+
+export default function Home() {
+
+  const [currentPage, setCurrentPage] = useState(1200)
+
+
+
+
+
+  return (
+    <></>
+  );
+}
